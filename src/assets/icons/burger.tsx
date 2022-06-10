dab4c6307dfc3ace83683e70c5221b3a7945a32a
alvarenga-allan <allan_alvarenga@msn.com>
2022-06-10 10:20:07 -0300

    chore: add icons components

--- src/assets/icons/burger.tsx
+++ src/assets/icons/burger.tsx
@@ -1,5 +1,19 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M21 7H3V5H21V7Z" fill="#212121"/>
-<path d="M21 13H3V11H21V13Z" fill="#212121"/>
-<path d="M3 19H21V17H3V19Z" fill="#212121"/>
-</svg>
+import React from 'react';
+
+function burguer(props: React.SVGProps<SVGSVGElement>) {
+  const { width = 24, height = 24, color = 'none' } = props;
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M21 7H3V5H21V7Z" fill="#212121" />
+      <path d="M21 13H3V11H21V13Z" fill="#212121" />
+      <path d="M3 19H21V17H3V19Z" fill="#212121" />
+    </svg>
+  );
+}
+export default burguer;
--- src/assets/icons/minus.tsx
+++ src/assets/icons/minus.tsx
@@ -1,3 +1,22 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M21 13H3V11H21V13Z" fill="#212121"/>
-</svg>
+import React from 'react';
+
+function minus(props: React.SVGProps<SVGSVGElement>) {
+  const { width = 24, height = 24, color = 'none' } = props;
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M21 13H3V11H21V13Z"
+        fill="#212121"
+      />
+    </svg>
+  );
+}
+export default minus;
