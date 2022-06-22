8b83e2511f2eee8a0dd671296ed89a6d312d8478
alvarenga-allan <allan_alvarenga@msn.com>
2022-06-22 15:33:27 -0300

    chore: add icons,flags and images

---
+++ src/assets/flags/poland.tsx
@@ -0,0 +1,4 @@
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="3" y="3" width="18" height="9" fill="#F5F5F5"/>
+<rect x="3" y="12" width="18" height="9" fill="#F44336"/>
+</svg>
